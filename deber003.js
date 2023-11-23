d7469039c7625e118e07fda12db6757a55e67b9f
Salome2112 <146013347+Salome2112@users.noreply.github.com>
2023-11-22 19:43:04 -0500

    cambio

---
+++ deber003.js
@@ -0,0 +1,12 @@
+'use strict'
+function validarTaxi( añoDelTaxi2013){
+    let tiempo = 10;
+    for (let i=10; i < añoDelTaxi2013; i++){
+        tiempo = tiempo - añoDelTaxi2013
+    }
+}
+ let vidaUtilDelTaxi =  Number(prompt("Ingresar año"));
+ let añosDelTaxi = Number(prompt("Ingresar año del taxi"));
+alert(true);
+
+
